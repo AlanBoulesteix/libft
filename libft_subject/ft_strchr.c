bbe867513ef2ab6d9f22b347f10fe0f1ecd44447
AlanBoulesteix <alan.boulesteix2312@gmail.com>
2022-10-28 18:22:46 +0200

    all functions added except calloc

--- libft_subject/ft_strchr.c
+++ libft_subject/ft_strchr.c
@@ -6,9 +6,11 @@
 /*   By: aboulest <aboulest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 23:34:56 by aboulest          #+#    #+#             */
-/*   Updated: 2022/10/27 23:34:58 by aboulest         ###   ########.fr       */
+/*   Updated: 2022/10/28 18:02:02 by aboulest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char    *ft_strchr(const char *s, int c)
 {
@@ -23,5 +25,19 @@ char    *ft_strchr(const char *s, int c)
             return (&ptr[i]);
         i++;
     }
-    return (&ptr[i]);
+    return (NULL);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int c;
+    char    *s = "Salut";
+
+    c = '';
+    printf ("La vraie fonction: %s\n", strchr(s, c));
+    printf ("Ma fonction: %s\n", ft_strchr(s, c));
+    return (0);
 }
--- libft_subject/ft_strnstr.c
+++ libft_subject/ft_strnstr.c
@@ -1,19 +1,34 @@
-char    *strnstr(const char *haystack, const char *needle, int len)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboulest <aboulest@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 16:05:01 by aboulest          #+#    #+#             */
+/*   Updated: 2022/10/28 18:21:47 by aboulest         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    int i;
-    int j;
+    size_t i;
+    size_t j;
 
     i = 0;
-    j = 0;
-
-    while (haystack[i])
+    if (!haystack)
+       return ((char *)haystack);
+    while (haystack[i] && i < len)
     {
-        while (needle[j])
-        {
-            if (needle[j] == haystack[i])
-            {
-                
-            }
-        }
+        j = 0;
+        while (haystack[i + j] == needle[j] && haystack[i + j] && i + j < len)
+            j++;
+        if (!needle[j])
+            return((char*)haystack + i);
+        i++;
     }
+    return(NULL);
 }
